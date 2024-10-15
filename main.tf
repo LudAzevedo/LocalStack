f510754d1b4ddbed6f16e97d0014a44848ed276c
Ludgero <ludgerorpt@gmail.com>
2024-10-15 15:18:17 -0300

    update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,11 @@ provider "aws" {
   secret_key                  = "test"
   region                      = "us-east-1"
   skip_credentials_validation = true
-  s3_force_path_style         = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+
+  s3_use_path_style = true # Forçar o uso do estilo de caminho ao invés de subdomínio
+
   endpoints {
     s3      = "http://localhost:4566"
     dynamodb = "http://localhost:4566"
@@ -24,3 +28,5 @@ resource "aws_dynamodb_table" "my_table" {
     type = "S"
   }
 }
+
+
