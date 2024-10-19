cf6fda9ae0fca4924cb4a20928a2ce996b98d490
Ludgero <ludgerorpt@gmail.com>
2024-10-18 22:38:03 -0300

    update provider

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  #alias                      = "localstack"
+  alias                      = "localstack"
   access_key                 = "test"
   secret_key                 = "test"
   region                     = "us-east-1"
